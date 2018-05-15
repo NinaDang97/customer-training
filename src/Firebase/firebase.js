48f25d62cd469249b697252c7e1c4fba384d7b12
Khanh Dang <31354481+NinaDang97@users.noreply.github.com>
2018-05-15 21:46:12 +0300

    Update firebase.js

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyA7nz5nNWFK488ViIOl2XJMff3E3xKzzeg",
-    authDomain: "my-project-1497126186658.firebaseapp.com",
-    databaseURL: "https://my-project-1497126186658.firebaseio.com",
-    projectId: "my-project-1497126186658",
-    storageBucket: "my-project-1497126186658.appspot.com",
-    messagingSenderId: "912038265317"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
 
 if(!firebase.apps.length){
